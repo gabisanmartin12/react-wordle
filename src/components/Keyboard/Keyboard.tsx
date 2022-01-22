0584e39764a310ff71e58b7e3b32b7c73e5f54d8
Gabriel San Martín <gabisanmartin12@gmail.com>
2022-01-22 02:07:46 -0300

    Add Keyboard's onClick event. Update keyboard's styles.

--- src/components/Keyboard/Keyboard.tsx
+++ src/components/Keyboard/Keyboard.tsx
@@ -1,57 +1,69 @@
-import { BackspaceIcon } from "@heroicons/react/outline";
+import { BackspaceIcon } from '@heroicons/react/outline';
+import { MouseEvent } from 'react';
+import { Key } from '../../types/keyboard';
 
 const KEYBOARD_DISTRIBUTION = [
   [
-    { code: "KeyQ", label: "Q" },
-    { code: "KeyW", label: "W" },
-    { code: "KeyE", label: "E" },
-    { code: "KeyR", label: "R" },
-    { code: "KeyT", label: "T" },
-    { code: "KeyY", label: "Y" },
-    { code: "KeyU", label: "U" },
-    { code: "KeyI", label: "I" },
-    { code: "KeyO", label: "O" },
-    { code: "KeyP", label: "P" },
+    { code: Key.Q, label: 'Q' },
+    { code: Key.W, label: 'W' },
+    { code: Key.E, label: 'E' },
+    { code: Key.R, label: 'R' },
+    { code: Key.T, label: 'T' },
+    { code: Key.Y, label: 'Y' },
+    { code: Key.U, label: 'U' },
+    { code: Key.I, label: 'I' },
+    { code: Key.O, label: 'O' },
+    { code: Key.P, label: 'P' },
   ],
   [
-    { code: "KeyA", label: "A" },
-    { code: "KeyS", label: "S" },
-    { code: "KeyD", label: "D" },
-    { code: "KeyF", label: "F" },
-    { code: "KeyG", label: "G" },
-    { code: "KeyH", label: "H" },
-    { code: "KeyJ", label: "J" },
-    { code: "KeyK", label: "K" },
-    { code: "KeyL", label: "L" },
+    { code: Key.A, label: 'A' },
+    { code: Key.S, label: 'S' },
+    { code: Key.D, label: 'D' },
+    { code: Key.F, label: 'F' },
+    { code: Key.G, label: 'G' },
+    { code: Key.H, label: 'H' },
+    { code: Key.J, label: 'J' },
+    { code: Key.K, label: 'K' },
+    { code: Key.L, label: 'L' },
   ],
   [
-    { code: "Enter", label: "ENTER" },
-    { code: "KeyL", label: "Z" },
-    { code: "KeyL", label: "X" },
-    { code: "KeyL", label: "C" },
-    { code: "KeyL", label: "V" },
-    { code: "KeyL", label: "B" },
-    { code: "KeyL", label: "N" },
-    { code: "KeyL", label: "M" },
-    { code: "Backspace", label: <BackspaceIcon className="w-8" /> },
+    { code: Key.Enter, label: 'ENTER' },
+    { code: Key.Z, label: 'Z' },
+    { code: Key.X, label: 'X' },
+    { code: Key.C, label: 'C' },
+    { code: Key.V, label: 'V' },
+    { code: Key.B, label: 'B' },
+    { code: Key.N, label: 'N' },
+    { code: Key.M, label: 'M' },
+    {
+      code: Key.Backspace,
+      label: <BackspaceIcon className="w-8 pointer-events-none" />,
+    },
   ],
 ];
 
-const Keyboard = () => (
-  <div className="w-full py-2 flex flex-col space-y-2 justify-between">
-    {KEYBOARD_DISTRIBUTION.map((row, index) => (
-      <div key={`row-${index}`} className="px-2 flex space-x-2 justify-center">
-        {row.map(({ code, label }) => (
-          <div
-            key={code}
-            className="h-14 px-3 flex-1 flex justify-center items-center rounded bg-gray-300 text-xs font-semibold cursor-pointer"
-          >
-            {label}
-          </div>
-        ))}
-      </div>
-    ))}
-  </div>
-);
+const Keyboard = () => {
+  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLDivElement;
+    if (target.id) {
+      let id = target.id as Key;
+      console.log(id);
+    }
+  };
+
+  return (
+    <div className="keyboard" onClick={handleClick}>
+      {KEYBOARD_DISTRIBUTION.map((row, index) => (
+        <div className="keyboard__row" key={`row-${index}`}>
+          {row.map(({ code, label }) => (
+            <div className="keyboard__key" key={code} id={code}>
+              {label}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Keyboard;
